d6c01fca7dd87216a5a22febceb96b48f5298926
sudeep <sudeep@iorta.in>
2023-01-28 19:28:54 +0530

    --- updated demo without threadpool

---
+++ index-worker.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const { Worker } = require("worker_threads");
+
+
+app.get("/non-blocking/", (req, res) => {
+  res.status(200).send("This page is non-blocking");
+});
+
+app.get("/blocking", async (req, res) => {
+  const worker = new Worker("./worker.js");
+  worker.on("message", (data) => {
+    res.status(200).send(`result is ${data}`);
+  });
+  worker.on("error", (msg) => {
+    res.status(404).send(`An error occurred: ${msg}`);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
